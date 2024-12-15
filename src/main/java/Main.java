5cc12ceb2aa61f84ea698c34949aebc8ebdcda7d
Mikolaj Bauc <mikolaj.bauc@gmail.com>
2024-12-15 11:37:05 +0100

    tuż przed obiektowością

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.io.PrintStream;
 
 public class Main {
     public static void main(String[] args) {
-        byte value1 = -128;
+       /* byte value1 = -128;
         System.out.println("Current value is " + value1);
 
         int myValue;                //deklaracja
@@ -52,8 +52,6 @@ public class Main {
         }
 
 
-
-
         int zmiennaGlobalna = 90;
         while(zmiennaGlobalna > 0) {
             zmiennaGlobalna--;
@@ -74,7 +72,69 @@ public class Main {
             System.out.println("Można jechać");
         } else {
             System.out.println("Nie wolno jechać");
+        }*/
+
+        /*for (int i = 0; i < 256; i++) {
+            char znak = (char) i;
+            System.out.println(i + " = " + znak);
         }
+
+        int speed = 0;
+
+        if (speed >= 1 && speed < 10) {
+            System.out.println("Info");
+        } else if (speed >= 10) {
+            System.out.println("Warning");
+        }*/
+
+        /*int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
+
+        for (int i = 0; i <=9; i++) {
+            System.out.println(array[i]);
+        }*/
+
+        /*int[] array2 = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println(array2[i]);
+        }*/
+
+        /*int[] newArray = new int[10];
+        for (int i = 0; i <= 9; i++) {
+            newArray[i] = i + 21;
+            System.out.println(newArray[i]);
+        }
+
+        String[] days = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
+
+        for (int i =0; i < days.length; i++) {
+            System.out.println(days[i]);
+        }*/
+
+        /*for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("Dla i =  " + i + ", j = " + j);
+                System.out.println("A zatem " + i + " * " + j + " to " + i*j);
+            }
+        }*/
+
+        /*char operacja = '*';
+        int a = 3, b = 9;
+        switch (operacja) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a/b);
+                break;
+            default:
+                System.out.println("Błędny znak lub wartość");
+        }*/
 
     }
 }
